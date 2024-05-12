40e3966d358df484977389a3054b57305808b340
H4CK3R-CODING <139951691+H4CK3R-CODING@users.noreply.github.com>
2024-05-12 11:20:57 +0530

    Starting Responsive of Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -24,20 +24,20 @@ const Navbar = () => {
     
   ]
   return (
-    <div className='static h-16 '>
+    <div className='static h-16'>
       <div className='fixed w-full h-16 flex justify-between items-center bg-gradient-to-r from-[#50FFB1] to-[#4FB286]'>
-      {/* <img className='w-40 h-16' src="./logowbc.png" alt="logowbc" /> */}
-      <Link to={"/"} className='text-white font-semibold italic p-2 m-2 text-3xl'>StudySpotlight</Link>
-      <div>
-        <ul className='flex justify-center items-center'>
-          {
-            data.map((ele,idx)=>{
-              return <NavBtn list={ele} key={idx}/>
-            })
-          }
-        </ul>
+        {/* <img className='w-40 h-16' src="./logowbc.png" alt="logowbc" /> */}
+        <Link to={"/"} className='text-white font-semibold italic p-2 m-2 text-3xl'>StudySpotlight</Link>
+        <div>
+          <ul className='flex justify-center items-center'>
+            {
+              data.map((ele,idx)=>{
+                return <NavBtn list={ele} key={idx}/>
+              })
+            }
+          </ul>
+        </div>
       </div>
-    </div>
     </div>
   )
 }
