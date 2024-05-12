5aef9cab3f15293358b60536fae5088595598fce
H4CK3R-CODING <139951691+H4CK3R-CODING@users.noreply.github.com>
2024-05-12 19:56:20 +0530

    update data.js

--- backend/utils/data.js
+++ backend/utils/data.js
@@ -34,6 +34,16 @@ const data =  {
     cse: {
         1: [
             {
+                subject: "Biology",
+                link: "https://drive.google.com/drive/folders/14RvXCwAzL3UDqSJMibI2uJwOEoYj9bQY?usp=sharing"
+            },
+            {
+                subject: "Programming for Problem Solving",
+                link: "https://drive.google.com/drive/folders/16QbdVwqw_Pdjmto8cZbJkoZDLpRyAkYx?usp=sharing"
+            },
+        ],
+        2: [
+            {
                 subject: "Math",
                 link: "https://www.google.com/"
             },
@@ -42,7 +52,27 @@ const data =  {
                 link: "https://www.google.com/"
             },
         ],
-        2: [
+        3: [
+            {
+                subject: "Math",
+                link: "https://www.google.com/"
+            },
+            {
+                subject: "Science",
+                link: "https://www.google.com/"
+            },
+        ],
+        4: [
+            {
+                subject: "Math",
+                link: "https://www.google.com/"
+            },
+            {
+                subject: "Science",
+                link: "https://www.google.com/"
+            },
+        ],
+        5: [
             {
                 subject: "Math",
                 link: "https://www.google.com/"
