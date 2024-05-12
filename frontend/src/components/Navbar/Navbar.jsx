1bde9e12c88212ba7ce8a8027e8048d8c66b38ae
H4CK3R-CODING <139951691+H4CK3R-CODING@users.noreply.github.com>
2024-05-12 13:03:30 +0530

    Complete Navbar responsiveness

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import { FaBars, FaTimes } from "react-icons/fa";
 import NavBtn from './NavBtn'
 import { Link } from 'react-router-dom'
+import "../../Style/Navbar.css";
 
 const Navbar = () => {
+  const navref = useRef();
+
+  const handleNavResponsive = () =>{
+    navref.current.classList.toggle("responsive-nav");
+  }
+
   const data = [
     {
       label: "Home",
@@ -24,19 +31,21 @@ const Navbar = () => {
     
   ]
   return (
-    <div className='static h-16'>
+    <div className='static h-16 z-20'>
       <div className='fixed w-full h-16 flex justify-between items-center bg-gradient-to-r from-[#50FFB1] to-[#4FB286]'>
         {/* <img className='w-40 h-16' src="./logowbc.png" alt="logowbc" /> */}
         <Link to={"/"} className='text-white font-semibold italic p-2 m-2 text-3xl'>StudySpotlight</Link>
-        <div>
-          <ul className='flex justify-center items-center'>
+        <div ref={navref} className='nav'>
+          <ul className='flex justify-center items-center flex-col md:flex-row'>
             {
               data.map((ele,idx)=>{
                 return <NavBtn list={ele} key={idx}/>
               })
             }
           </ul>
+          <button className='nav-btn closeBtn' onClick={handleNavResponsive}><FaTimes /></button>
         </div>
+        <button className='nav-btn ' onClick={handleNavResponsive}><FaBars /></button>
       </div>
     </div>
   )
--- frontend/src/pages/Home/Btn.jsx
+++ frontend/src/pages/Home/Btn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Btn = ({btninfo}) => {
   return (
-    <button className='drop-shadow-2xl bg-black brightness-90 text-white border-2 border-black p-2 font-semibold m-1 rounded-lg' onClick={btninfo.onclick}>{btninfo.label}</button>
+    <button className='drop-shadow-2xl bg-black brightness-90 text-white border-2 border-black p-2 font-semibold m-1 rounded-lg -z-10' onClick={btninfo.onclick}>{btninfo.label}</button>
   )
 }
 
