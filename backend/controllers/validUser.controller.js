932e8fa94e6f0a013407353c0ea8531f42f55632
H4CK3R-CODING <139951691+H4CK3R-CODING@users.noreply.github.com>
2024-05-13 19:14:33 +0530

    add mail sender but facing error

--- backend/controllers/validUser.controller.js
+++ backend/controllers/validUser.controller.js
@@ -2,6 +2,17 @@ import validUserAuth from "../zod/validUserAuth.js";
 import validUserModel from "../models/user/validUser.model.js"
 import User from "../models/user/user.model.js";
 import bcrypt from 'bcrypt'
+import nodemailer from "nodemailer"
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
 const validUser = async (req,res)=>{
     try {
@@ -37,6 +48,28 @@ const validUser = async (req,res)=>{
                 username,
                 password: hashPassword
             })
+
+            // send mail to user
+            const mailOption = {
+                from: process.env.SMTP_MAIL,
+                to: username,
+                subject: "check mail",
+                message: "How are you"
+            }
+
+            await transporter.sendMail(mailOption,(error, info)=>{
+                if(error){
+                    console.log(error.message)
+                }
+                else{
+                    console.log("mail sent")
+                }
+            })
+
+
+
+
+
             res.status(200).json({
                 username,
                 password
