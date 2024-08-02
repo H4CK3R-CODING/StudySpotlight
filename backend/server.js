5ba26fc58842d13a53a016d748f25e602576087b
H4CK3R-CODING <139951691+H4CK3R-CODING@users.noreply.github.com>
2024-08-02 12:39:58 +0530

    update backend

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/v1/",mainRouter);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-})
+// app.get("*", (req,res)=>{
+//     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// })
 
 
 app.listen(PORT,async ()=>{
